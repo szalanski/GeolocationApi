715baec1a3e74564db8a91e94a9e93c49ea0a832
Marcin <szalanski91@gmail.com>
2022-08-04 15:06:20 +0200

    Add BaseResponse

---
+++ GeolocationApi/GeolocationApi.Application/Responses/BaseResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace GeolocationApi.Application.Responses
+{
+    public abstract record BaseResponse
+    {
+        public bool Succeeded { get; set; }
+        public string Message { get; set; }
+        public List<string> ValidationErrors { get; set; }
+
+        public BaseResponse(bool succeeded = true, string message = "", List<string> validationErrors = null)
+        {
+            Succeeded = succeeded;
+            Message = message;
+            ValidationErrors = validationErrors;
+        }
+    }
+    
+}
